5947330b61d24b7068dd1e9483352be0affbff4b
FuRz3bly <dafurydragon25@gmail.com>
2024-05-22 16:15:05 +0800

    initial commit

---
+++ app/signupPage/communitySignup.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const communitySignup = () => {
+  return (
+    <View>
+      <Text>communitySignup</Text>
+    </View>
+  )
+}
+
+export default communitySignup
+
+const styles = StyleSheet.create({})
---
+++ components/global/colors.js
@@ -0,0 +1,6 @@
+export default{
+    black:'#00000',
+    white: '#fff',
+    lightgray: '#efefe',
+    green: '#57b378',
+}
